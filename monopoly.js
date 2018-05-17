cd707a4c1be4c53ca22ea2d1acab6de3eee4d1ba
elasticMongrel <33241429+elasticMongrel@users.noreply.github.com>
2018-05-16 22:25:53 -0400

    Update monopoly.js
    added rollDice(), player object, var'd die1 and die2, and updatePosition()

--- monopoly.js
+++ monopoly.js
@@ -1,7 +1,28 @@
 //javaScript stuff for monopoly
 
+var player = {
+	position: 0
+};
+
+var die1 = 0;
+var die2 = 0;
+
 //when user clicks on div, open the popup
 function myFunction() {
 	var popup = document.getElementById("myPopup");
 	popup.classList.toggle("show");
+	rollDice();
+}
+
+function rollDice() {
+	die1 = Math.floor((Math.random() * 6) + 1);
+	die2 = Math.floor((Math.random() * 6) + 1);
+	document.getElementById("die1-display").innerHTML = die1.toString(); //innerHTML changes the HTML within that element via ID
+	document.getElementById("die2-display").innerHTML = die2.toString();
+}
+
+function updatePosition() {
+	for(i = 0; i < 40; i++) { // a way to cycle through all board ID's
+		document.getElementById("cell" + i)
+	}
 }
