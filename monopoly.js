e811586a18b931797cc696e41d3b7f7e05a7d19d
elasticMongrel <33241429+elasticMongrel@users.noreply.github.com>
2018-05-21 12:53:01 -0400

    Update monopoly.js
    added doubles and being put in jail after getting 3 doubles. There is currently no way to get out of jail, and the third person to roll a double is put in jail (this is for testing purposes as it's almost impossible to roll 3 doubles in a row. This must be fixed for the final version, and returning the variable doublesRolled to 0 after each player's turn must also be added).

--- monopoly.js
+++ monopoly.js
@@ -3,29 +3,38 @@
 var player1 = {
 	position: 0,
 	color: "red",
-	isTurn: true
+	isTurn: true,
+	inJail: false
 };
 
 var player2 = {
 	position: 0,
 	color: "blue",
-	isTurn: false
+	isTurn: false,
+	inJail: false
 };
 
 var player3 = {
 	position: 0,
 	color: "yellow",
-	isTurn: false
+	isTurn: false,
+	inJail: false
 };
 
 var player4 = {
 	position: 0,
 	color: "green",
-	isTurn: false
+	isTurn: false,
+	inJail: false
+};
+
+var kentuckyAvenue = {
+	
 };
 
 var die1 = 0;
 var die2 = 0;
+var doublesRolled = 0;
 
 //when user clicks on div, open the popup
 function myFunction() {
@@ -44,56 +53,188 @@ function updatePosition() {
 	//	document.getElementById("cell" + i)
 	//}
 	if(player1.isTurn === true){
-		var elem = document.getElementById("cell" + player1.position + "_1");
-		elem.classList.remove("piece1");
+		var oldElem = document.getElementById("cell" + player1.position + "_1");
+		oldElem.classList.remove("piece1");
 		if(player1.position + die1 + die2 > 39){
 			player1.position = player1.position + die1 + die2 - 40;
 		} else {
 			player1.position += (die1 + die2);
 		}
-		elem = document.getElementById("cell" + player1.position + "_1");
-		elem.classList.add("piece1");
-		player1.isTurn = false;
-		player2.isTurn = true;
+		var newElem = document.getElementById("cell" + player1.position + "_1");
+		newElem.classList.add("piece1");
+		if(die1 === die2){
+			doublesRolled++;
+			if(doublesRolled === 3){
+				alert("You rolled 3 doubles! You're going to jail!");
+				player1.inJail = true;
+				player1.position = 10;
+				newElem.classList.remove("piece1");
+				newElem = document.getElementById("cell10_1");
+				newElem.classList.add("piece1");
+				
+				player1.isTurn = false;
+				if(player2.inJail === false){
+					player2.isTurn = true;
+				} else if(player3.inJail === false){
+					player3.isTurn = true;
+				} else if(player4.inJail === false){
+					player4.isTurn = true;
+				} else {
+					player1.isTurn = true;
+				}
+			} else {
+				alert("You rolled doubles! Roll again.");
+			}
+		} else {
+			player1.isTurn = false;
+			if(player2.inJail === false){
+				player2.isTurn = true;
+			} else if(player3.inJail === false){
+				player3.isTurn = true;
+			} else if(player4.inJail === false){
+				player4.isTurn = true;
+			} else {
+				player1.isTurn = true;
+			}
+		}
 	}
 	else if(player2.isTurn === true){
-		var elem = document.getElementById("cell" + player2.position + "_2");
-		elem.classList.remove("piece2");
+		var oldElem = document.getElementById("cell" + player2.position + "_2");
+		oldElem.classList.remove("piece2");
 		if(player2.position + die1 + die2 > 39){
 			player2.position = player2.position + die1 + die2 - 40;
 		} else {
 			player2.position += (die1 + die2);
 		}
-		elem = document.getElementById("cell" + player2.position + "_2");
-		elem.classList.add("piece2");
-		player2.isTurn = false;
-		player3.isTurn = true;
+		var newElem = document.getElementById("cell" + player2.position + "_2");
+		newElem.classList.add("piece2");
+		if(die1 === die2){
+			doublesRolled++;
+			if(doublesRolled === 3){
+				alert("You rolled 3 doubles! You're going to jail!");
+				player2.inJail = true;
+				player2.position = 10;
+				newElem.classList.remove("piece2");
+				newElem = document.getElementById("cell10_2");
+				newElem.classList.add("piece2");
+				
+				player2.isTurn = false;
+				if(player3.inJail === false){
+					player3.isTurn = true;
+				} else if(player4.inJail === false){
+					player4.isTurn = true;
+				} else if(player1.inJail === false){
+					player1.isTurn = true;
+				} else {
+					player2.isTurn = true;
+				}
+			} else {
+				alert("You rolled doubles! Roll again.");
+			}
+		}  else {
+			player2.isTurn = false;
+			if(player3.inJail === false){
+				player3.isTurn = true;
+			} else if(player4.inJail === false){
+				player4.isTurn = true;
+			} else if(player1.inJail === false){
+				player1.isTurn = true;
+			} else {
+				player2.isTurn = true;
+			}
+		}
 	}
 	else if(player3.isTurn === true){
-		var elem = document.getElementById("cell" + player3.position + "_3");
-		elem.classList.remove("piece3");
+		var oldElem = document.getElementById("cell" + player3.position + "_3");
+		oldElem.classList.remove("piece3");
 		if(player3.position + die1 + die2 > 39){
 			player3.position = player3.position + die1 + die2 - 40;
 		} else {
 			player3.position += (die1 + die2);
 		}
-		elem = document.getElementById("cell" + player3.position + "_3");
-		elem.classList.add("piece3");
-		player3.isTurn = false;
-		player4.isTurn = true;
+		var newElem = document.getElementById("cell" + player3.position + "_3");
+		newElem.classList.add("piece3");
+		if(die1 === die2){
+			doublesRolled++;
+			if(doublesRolled === 3){
+				alert("You rolled 3 doubles! You're going to jail!");
+				player3.inJail = true;
+				player3.position = 10;
+				newElem.classList.remove("piece3");
+				newElem = document.getElementById("cell10_3");
+				newElem.classList.add("piece3");
+				
+				player3.isTurn = false;
+				if(player4.inJail === false){
+					player4.isTurn = true;
+				} else if(player1.inJail === false){
+					player1.isTurn = true;
+				} else if(player2.inJail === false){
+					player2.isTurn = true;
+				} else {
+					player3.isTurn = true;
+				}
+			} else {
+				alert("You rolled doubles! Roll again.");
+			}
+		} else {
+			player3.isTurn = false;
+			if(player4.inJail === false){
+				player4.isTurn = true;
+			} else if(player1.inJail === false){
+				player1.isTurn = true;
+			} else if(player2.inJail === false){
+				player2.isTurn = true;
+			} else {
+				player3.isTurn = true;
+			}
+		}
 	}
 	else if(player4.isTurn === true){
-		var elem = document.getElementById("cell" + player4.position + "_4");
-		elem.classList.remove("piece4");
+		var oldElem = document.getElementById("cell" + player4.position + "_4");
+		oldElem.classList.remove("piece4");
 		if(player4.position + die1 + die2 > 39){
 			player4.position = player4.position + die1 + die2 - 40;
 		} else {
 			player4.position += (die1 + die2);
 		}
-		elem = document.getElementById("cell" + player4.position + "_4");
-		elem.classList.add("piece4");
-		player4.isTurn = false;
-		player1.isTurn = true;
+		var newElem = document.getElementById("cell" + player4.position + "_4");
+		newElem.classList.add("piece4");
+		if(die1 === die2){
+			doublesRolled++;
+			if(doublesRolled === 3){
+				alert("You rolled 3 doubles! You're going to jail!");
+				player4.inJail = true;
+				player4.position = 10;
+				newElem.classList.remove("piece4");
+				newElem = document.getElementById("cell10_4");
+				newElem.classList.add("piece4");
+				
+				player4.isTurn = false;
+				if(player1.inJail === false){
+					player1.isTurn = true;
+				} else if(player2.inJail === false){
+					player2.isTurn = true;
+				} else if(player3.inJail === false){
+					player3.isTurn = true;
+				} else {
+					player4.isTurn = true;
+			}
+			} else {
+				alert("You rolled doubles! Roll again.");
+			}
+		} else {
+			player4.isTurn = false;
+			if(player1.inJail === false){
+				player1.isTurn = true;
+			} else if(player2.inJail === false){
+				player2.isTurn = true;
+			} else if(player3.inJail === false){
+				player3.isTurn = true;
+			} else {
+				player4.isTurn = true;
+			}
+		}
 	}
 }
 
@@ -116,3 +257,7 @@ function rollDice() {
 		}
 	}
 }*/
+
+function putInJail() {
+	
+}
